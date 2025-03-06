232ddea0e2c11d6f579c7b5de1bc49826ac1a36e
Fin Wilson <s2750265@uhtred.inf.ed.ac.uk>
2025-03-06 22:39:44 +0000

    Allowed for finer hyper parameter control in runTens

--- runTens.sh
+++ runTens.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Define variables
+# Define hyperparameters
 NumEpochs=1000
 NumEpisodes=3
-Dir="/disk/scratch//mlp_cw4/"
+learning_rate=3e-4
+clip_epsilon=0.2
+gamma=0.99
+update_epochs=4
+value_loss_coef=0.5
+entropy_coef=0.1
+
+# Directory for your Python code
+Dir="/disk/scratch/mlp_cw4/"
 InputFileDirectory=""
 OutputFileDirectory=""
 
-# Call the Python script with arguments
-python "$Dir/main.py" --epochs $NumEpochs --episodes $NumEpisodes --input_dir "$InputFileDirectory" --output_dir "$OutputFileDirectory"
+# Run the Python script with all hyperparameters and correct input names
+python "$Dir/main.py" --epochs $NumEpochs --episodes $NumEpisodes \
+--learning_rate $learning_rate --clip_epsilon $clip_epsilon --gamma $gamma \
+--update_epochs $update_epochs --value_loss_coef $value_loss_coef --entropy_coef $entropy_coef \
+--input_dir "$InputFileDirectory" --output_dir "$OutputFileDirectory"
