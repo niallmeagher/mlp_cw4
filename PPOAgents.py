c250558ac3c0f0e3895d9bb20c3af261d3423ae5
Matthew Collins <s2686742@ed.ac.uk>
2025-03-08 12:48:58 +0000

    Test

--- PPOAgents.py
+++ PPOAgents.py
@@ -11,7 +11,7 @@ import subprocess
 import os
 import glob
 import difflib
-
+import uuid
 import csv
 import tempfile
 import concurrent.futures
@@ -166,10 +166,11 @@ class PPOAgent:
                 return None
         '''
         print("Running")
-        
+        episode_id = uuid.uuid4().hex
         
         self.modify_csv(os.path.join(work_folder, "Data.csv"),os.path.join(work_folder, "Data.csv"), topk_indices, 'NF')
         self.modify_first_column(os.path.join(work_folder, "Data.dat"),os.path.join(work_folder, "Data.dat"), topk_indices, is_csv=False)
+        temp_output = tempfile.mkdtemp(prefix=f"cell2fire_output_{episode_id}_", dir = "/tmp")
         
         if stochastic == True:
             FPL = str(np.round(np.random.uniform(0.5, 3.0), 2))
@@ -197,7 +198,7 @@ class PPOAgent:
             cmd = [
                 f"{HOME_DIR}./Cell2Fire",
                 "--input-instance-folder", work_folder,
-                "--output-folder", output_folder,
+                "--output-folder", temp_output,
                 "--ignitions",
                 "--sim-years", str(1),
                 "--nsims", str(num_grids),
@@ -255,10 +256,10 @@ class PPOAgent:
             return None
         
         base_grids_folder = os.path.join(output_folder_base, "Grids")
-        print(os.listdir(output_folder),os.listdir(output_folder_base))
+        print(os.listdir(temp_output),os.listdir(output_folder_base))
         contents2 = os.listdir(base_grids_folder)
         print(contents2)
-        firebreak_grids_folder = os.path.join(output_folder, "Grids")
+        firebreak_grids_folder = os.path.join(temp_output, "Grids")
         computed_values = []
         contents = os.listdir(firebreak_grids_folder)
         print(contents)
