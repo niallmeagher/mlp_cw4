c7d352625c3be7659cd60daf827aa56290d69642
Matthew Collins <s2686742@ed.ac.uk>
2025-03-09 12:01:20 +0000

    Test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import shutil
 import numpy as np
 import csv
 import argparse
+import time
 import uuid
 import tempfile
 from concurrent.futures import ThreadPoolExecutor as TPE
@@ -120,6 +121,7 @@ def read_multi_channel_asc(files, header_lines=6):
 def simulate_single_episode(agent, state, tabular_tensor, mask, input_folder):
     # Create a temporary working directory for this episode
     print("initial")
+    start_time = time.time()
     episode_id = uuid.uuid4().hex
     testing = "/tmp/"
     #temp_work_dir = tempfile.mkdtemp(prefix=f"cell2fire_input_{episode_id} /", dir = HOME_DIR)
@@ -127,6 +129,9 @@ def simulate_single_episode(agent, state, tabular_tensor, mask, input_folder):
     os.mkdir(temp_work_dir)
     temp_output_dir = tempfile.mkdtemp(prefix=f"cell2fire_output_{episode_id}", dir = testing)
     temp_output_base_dir = tempfile.mkdtemp(prefix=f"cell2fire_output_base_{episode_id}", dir = testing)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.4f} seconds")
     
     try:
         shutil.copytree(input_folder, temp_work_dir, dirs_exist_ok = True)
