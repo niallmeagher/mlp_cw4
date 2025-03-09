68a8584df4d050a25af2014abf5ccc16a639b932
Matthew Collins <s2686742@ed.ac.uk>
2025-03-09 15:18:00 +0000

    Test

--- main.py
+++ main.py
@@ -288,7 +288,7 @@ def main(args, start_epoch=0, checkpoint_path=None):
         '''
         print("EPISODES:", episodes_per_epoch)
         start_time = time.time()
-        with TPE(max_workers=episodes_per_epoch) as executor:
+        with PPE(max_workers=mp.cpu_count()) as executor:
             print("Executing")
             futures = [executor.submit(simulate_single_episode, agent,
                                    tensor_input.clone(), tabular_tensor, mask, input_folder_final)
